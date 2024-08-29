cb38bdf4c2dc4d32b92a15b6285232c4429f0d43
gyver5681 <davemarker@gmail.com>
2024-08-29 08:59:26 -0400

    Fixed Gamerule Application
    Addon no longer subscribes to the gameRuleChange event, and instead now relies on a dedicated form interface.

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-For Bedrock Realms; This addon will save any gamerule changes made using commands as a dynamic property on the world and reload them when the world reloads
+# Overview
+
+For Bedrock Realms; This addon will save custom gamerules as a dynamic property on the world and apply them via the gamerule command when the first player logs into an unloaded world.
+
+To use, add a tag named "GameruleUser" to the player and Use an amethyst crystal that's been renamed "gamerules".
+
+# Functions
+
+When the crystal is used, a Main Menu opens with the following options:
+
+## Add Gamerules
+
+This form lists all gamerules that have not been added to the addon. Rules are in alphabetical order for convenience. Select one and the gamerule will be added to the addon with its current default value. The form will reload so that multiple gamerules can be added faster. Close the form without making a selection to return to the Main Menu.
+
+## Edit Gamerules
+
+This form loads all custom gamerules with the value currently stored on the world. Pressing the Submit button will write the changes to world data. Cancelling the form will throw out any changes. Changes made are NOT automatically applied to the loaded world.
+
+## Show Saved Gamerules
+
+This option will display the raw JSON data saved on the world.
+
+## Apply Saved Gamerules
+
+This option will go through each saved gamerule and apply them to the active game via the gamerule command.
+
+## Clear Saved Gamerules
+
+This option will remove all saved gamerules from the saved data on the world. This will not affect the loaded game, but when the world is reloaded it will revert to its default behavior.
--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,50 +1,232 @@
 // scripts/main.ts
-import { world } from "@minecraft/server";
-var gyverGameruleName = "gyver:gamerules";
-var gyverGameruleLoadedName = "gyver:gamerulesloaded";
-function getGamerulesLoaded() {
-  var returnValue = false;
-  const gameruleLoaded = world.getDynamicProperty(gyverGameruleLoadedName);
-  if (typeof gameruleLoaded === "boolean") {
-    returnValue = gameruleLoaded;
-  }
-  return returnValue;
+import { GameRule, system, world } from "@minecraft/server";
+import { ActionFormData, MessageFormData, ModalFormData } from "@minecraft/server-ui";
+var gyverGameruleName = "GyverGamerules";
+var tagGameruleUser = "GameruleUser";
+var namespaceGrSaver = "grsaver";
+var grSaverMainMenu = `${namespaceGrSaver}:mainMenu`;
+var grSaverAddRule = `${namespaceGrSaver}:addRule`;
+function setSavedGameRules(gamerules) {
+  world.setDynamicProperty(gyverGameruleName, JSON.stringify(gamerules));
 }
-function setGamerulesLoaded(value) {
-  world.setDynamicProperty(gyverGameruleLoadedName, value);
+function getSavedGameRules() {
+  let jsonGameRules = world.getDynamicProperty(gyverGameruleName);
+  if (jsonGameRules) {
+    let savedGameRules = JSON.parse(jsonGameRules);
+    if (savedGameRules) {
+      return savedGameRules;
+    }
+  }
+}
+function getSavedNames(savedRules) {
+  let returnData = [];
+  savedRules.forEach((cachedRule) => {
+    returnData.push(cachedRule.name);
+  });
+  return returnData;
+}
+function updateSavedGameRules(updRule) {
+  const cachedRules = getSavedGameRules();
+  var newCachedRules = [];
+  var newRule = true;
+  if (cachedRules) {
+    for (var index = 0; index < cachedRules.length; index++) {
+      if (cachedRules[index].name == updRule.name) {
+        newCachedRules.push(updRule);
+        newRule = false;
+      } else {
+        newCachedRules.push(cachedRules[index]);
+      }
+    }
+  }
+  if (newRule) {
+    newCachedRules.push(updRule);
+    newCachedRules.sort((a, b) => a.name < b.name ? -1 : 1);
+  }
+  setSavedGameRules(newCachedRules);
+}
+function clearSavedGameRules() {
+  setSavedGameRules([]);
 }
 function applySavedGamerules() {
-  const cachedRulesJSON = world.getDynamicProperty(gyverGameruleName);
-  if (typeof cachedRulesJSON === "string") {
-    let cachedRules = JSON.parse(cachedRulesJSON);
-    Object.entries(cachedRules).forEach(([key, value]) => {
-      world.getDimension("overworld").runCommandAsync(`gamerule ${key} ${value}`);
+  const cachedRules = getSavedGameRules();
+  if (cachedRules) {
+    for (let index = 0; index < cachedRules.length; index++) {
+      world.getDimension("overworld").runCommandAsync(`gamerule ${cachedRules[index].name} ${cachedRules[index].value}`);
+    }
+    world.sendMessage("Custom Gamerules Loaded");
+  }
+}
+function applySavedGamerulesByPlayer(player) {
+  const cachedRules = getSavedGameRules();
+  if (cachedRules) {
+    for (let index = 0; index < cachedRules.length; index++) {
+      player.sendMessage(`running: "gamerule ${cachedRules[index].name} ${cachedRules[index].value}"`);
+      world.getDimension("overworld").runCommandAsync(`gamerule ${cachedRules[index].name} ${cachedRules[index].value}`);
+    }
+    player.sendMessage("Custom Gamerules Loaded");
+  }
+}
+function showEditGameRules(player) {
+  const savedRules = getSavedGameRules();
+  if (savedRules) {
+    const gameRulesEditorUi = new ModalFormData();
+    gameRulesEditorUi.title("Custom Game Rules");
+    for (const rule of savedRules) {
+      switch (typeof rule.value) {
+        case "boolean": {
+          gameRulesEditorUi.toggle(rule.name, rule.value);
+          break;
+        }
+        case "number": {
+          gameRulesEditorUi.textField(rule.name, "", rule.value.toString());
+          break;
+        }
+      }
+    }
+    gameRulesEditorUi.show(player).then((results) => {
+      if (!results.canceled && results.formValues) {
+        let newRules = [];
+        for (let index = 0; index < results.formValues.length; index++) {
+          switch (typeof results.formValues[index]) {
+            case "boolean": {
+              newRules.push({ name: savedRules[index].name, value: results.formValues[index] });
+              break;
+            }
+            case "string": {
+              newRules.push({ name: savedRules[index].name, value: parseInt(results.formValues[index]) });
+              break;
+            }
+          }
+        }
+        setSavedGameRules(newRules);
+      }
+      returnToGameruleMain(player);
+    });
+  } else {
+    const showNoGameRulesUi = new MessageFormData().title("").body("No Custom Game Rules").button1("Ok").button2("Ok");
+    showNoGameRulesUi.show(player).then(() => {
+      returnToGameruleMain(player);
     });
   }
 }
-world.afterEvents.worldInitialize.subscribe((event) => {
-  setGamerulesLoaded(false);
-});
+function showAddGameRules(player) {
+  const addRulesUi = new ActionFormData();
+  addRulesUi.title("Add Game Rules");
+  addRulesUi.body(
+    "Below are Game Rules that aren't being customized via this addon. Select on to add it to the customization form."
+  );
+  const savedRules = getSavedGameRules() || [];
+  const savedNames = getSavedNames(savedRules);
+  let gamerules = Object.entries(GameRule);
+  gamerules.sort((a, b) => a[1].toString().toUpperCase() < b[1].toString().toUpperCase() ? -1 : 1);
+  let defaultRules = [];
+  for (const [Name, Value] of gamerules) {
+    let addButton = true;
+    if (savedNames.length > 0) {
+      for (const savedName of savedNames) {
+        if (savedName === Value.toString()) {
+          addButton = false;
+          break;
+        }
+      }
+    }
+    if (addButton) {
+      addRulesUi.button(Value);
+      defaultRules.push([Value.toString(), Value]);
+    }
+  }
+  addRulesUi.show(player).then((response) => {
+    if (typeof response.selection === "number") {
+      const selectedValue = defaultRules[response.selection][1];
+      updateSavedGameRules({
+        name: selectedValue.toString(),
+        value: world.gameRules[selectedValue]
+      });
+      returnToAddRules(player);
+    } else {
+      returnToGameruleMain(player);
+    }
+  });
+}
+function showGameruleMainMenu(player) {
+  const mainMenuUi = new ActionFormData();
+  mainMenuUi.title("Gamerule Menu");
+  mainMenuUi.body("");
+  mainMenuUi.button("add gamerules");
+  mainMenuUi.button("edit gamerules");
+  mainMenuUi.button("show saved gamerules");
+  mainMenuUi.button("apply saved gamerules");
+  mainMenuUi.button("clear saved gamerules");
+  mainMenuUi.show(player).then((response) => {
+    if (typeof response.selection === "number") {
+      switch (response.selection) {
+        case 0: {
+          showAddGameRules(player);
+          break;
+        }
+        case 1: {
+          showEditGameRules(player);
+          break;
+        }
+        case 2: {
+          const cachedRulesJSON = world.getDynamicProperty(gyverGameruleName);
+          const showGamerulesUi = new MessageFormData();
+          showGamerulesUi.title("");
+          showGamerulesUi.body(`GyverGamerules: ${cachedRulesJSON}`);
+          showGamerulesUi.button1("Ok");
+          showGamerulesUi.button2("Ok");
+          showGamerulesUi.show(player).then(() => {
+            returnToGameruleMain(player);
+          });
+          break;
+        }
+        case 3: {
+          applySavedGamerulesByPlayer(player);
+          returnToGameruleMain(player);
+          break;
+        }
+        case 4: {
+          clearSavedGameRules();
+          returnToGameruleMain(player);
+          break;
+        }
+      }
+    }
+  });
+}
+function returnToGameruleMain(player) {
+  player.dimension.runCommand(`scriptevent ${grSaverMainMenu} ${player.name}`);
+}
+function returnToAddRules(player) {
+  player.dimension.runCommand(`scriptevent ${grSaverAddRule} ${player.name}`);
+}
+function reloadForm(event) {
+  switch (event.id) {
+    case grSaverMainMenu: {
+      const player = world.getPlayers({ name: event.message })[0];
+      showGameruleMainMenu(player);
+      break;
+    }
+    case grSaverAddRule: {
+      const player = world.getPlayers({ name: event.message })[0];
+      showAddGameRules(player);
+      break;
+    }
+  }
+}
 var onPlayerJoin = world.afterEvents.playerJoin.subscribe((event) => {
-  const player = world.getPlayers({ name: event.playerName });
-  if (getGamerulesLoaded()) {
-    world.afterEvents.playerJoin.unsubscribe(onPlayerJoin);
-  } else {
-    applySavedGamerules();
+  system.runTimeout(applySavedGamerules, 200);
+  world.afterEvents.playerJoin.unsubscribe(onPlayerJoin);
+});
+world.afterEvents.itemUse.subscribe((event) => {
+  const { source, itemStack } = event;
+  if (source.hasTag(tagGameruleUser)) {
+    if (itemStack.typeId === "minecraft:amethyst_shard" && itemStack.nameTag === "gamerules") {
+      showGameruleMainMenu(source);
+    }
   }
 });
-world.afterEvents.gameRuleChange.subscribe((event) => {
-  let cachedRules;
-  const cachedRulesJSONold = world.getDynamicProperty(gyverGameruleName);
-  if (typeof cachedRulesJSONold === "string") {
-    cachedRules = JSON.parse(cachedRulesJSONold);
-    cachedRules[event.rule] = event.value;
-  }
-  const cachedRulesJSON = JSON.stringify(cachedRules);
-  if (typeof cachedRulesJSON === "string") {
-    world.setDynamicProperty(gyverGameruleName, cachedRulesJSON);
-  }
-});
-console.log("gamerule-saver loaded!");
+system.afterEvents.scriptEventReceive.subscribe(reloadForm, { namespaces: [namespaceGrSaver] });
 
 //# sourceMappingURL=../debug/main.js.map
